0a2b883b15e6ad5c26554d9517fc4565684e481f
Sirius Guo <guotuo1024@gmail.com>
2018-05-24 16:13:46 +0800

    add debug file
    debug

---
+++ JishuitanHuilongguan/JishuitanHuilongguan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JishuitanHuilongguan
+//
+//  Created by Tuo on 2018/5/24.
+//  Copyright © 2018年 Tuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
